e183efeaf5f5a0c01098868d388c12d967e2d61b
Hinchliffe_Miranda <Hinchliffe_Miranda@G-02-22>
2021-09-15 10:20:40 -0400

    Add project files.

---
+++ CONDITIONAL STATEMENTS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONDITIONAL_STATEMENTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pepperLength = 4;
+
+            string message = (pepperLength >= 3.5) ? "Are you ready!" : "Just wait a little while longer";
+
+            Console.WriteLine(message);
+        }
+    }
+}
